633d8c7d6e5e83a334d7ac691d867f63ee33df01
Cristian Sierra <crijosicar@hotmail.com>
2025-06-22 18:18:15 -0400

    Updated adapter options

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,11 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
