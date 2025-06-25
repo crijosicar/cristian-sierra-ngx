268346bb352f273926978cbaff235410ba772d1b
Cristian Sierra <crijosicar@hotmail.com>
2025-06-24 21:25:31 -0400

    added ssr

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,3 +4,16 @@ process.on('exit', () => {
 	console.log('Exiting process...');
 	contactWorker.close();
 });
+
+
+// /** @type {import('@sveltejs/kit').[Handle](https://kit.svelte.dev/docs/types#public-types-handle)} */
+// export async function handle({ event, resolve }) {
+//   // true when the request is for a `__data.json` endpoint
+//   // https://kit.svelte.dev/docs/types#public-types-requestevent
+//   if (event.isDataRequest) {
+//     return new Response(null, { status: 200 });
+//   }
+
+//   const response = await resolve(event);
+//   return response;
+// }
