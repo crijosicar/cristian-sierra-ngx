7da3432defa863413a4c210cfd1162e5dd45262f
Cristian Sierra <crijosicar@hotmail.com>
2025-06-25 18:34:53 -0400

    Updated deployment options

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,6 @@
 import { contactWorker } from '$lib/workers/contactWorker';
 
-process.on('exit', () => {
-	console.log('Exiting process...');
+process.on('sveltekit:shutdown', async (reason) => {
+  	console.log(`Exiting process... with reason ${JSON.stringify(reason, Object.getOwnPropertyNames(reason))}`);
 	contactWorker.close();
 });
-
-
-// /** @type {import('@sveltejs/kit').[Handle](https://kit.svelte.dev/docs/types#public-types-handle)} */
-// export async function handle({ event, resolve }) {
-//   // true when the request is for a `__data.json` endpoint
-//   // https://kit.svelte.dev/docs/types#public-types-requestevent
-//   if (event.isDataRequest) {
-//     return new Response(null, { status: 200 });
-//   }
-
-//   const response = await resolve(event);
-//   return response;
-// }
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,14 +6,15 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
+			out: 'build',
+			precompress: true,
 			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+		prerender: {
+			entries: ['*']
 		}
 	}
 };
