64e32de43cc4c928b9ad36a1ed61ec7448517aa7
Cristian Sierra <crijosicar@hotmail.com>
2025-08-29 18:11:08 -0400

    Updated validation

--- src/lib/services/turnstile.service.ts
+++ src/lib/services/turnstile.service.ts
@@ -5,25 +5,25 @@ interface TokenValidateResponse {
 	cdata: string;
 }
 
-export const validateToken = async (token: string, secret: string) => {
-	const response = await fetch(
-		'https://challenges.cloudflare.com/turnstile/v0/siteverify',
-		{
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json',
-			},
-			body: JSON.stringify({
-				response: token,
-				secret: secret,
-			}),
+export const validateToken = async (
+	token: string,
+	secret: string
+): Promise<{ success: boolean; error: string }> => {
+	const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json'
 		},
-	);
+		body: JSON.stringify({
+			response: token,
+			secret
+		})
+	});
 
 	const data: TokenValidateResponse = await response.json();
 
 	return {
 		success: data.success,
-		error: data['error-codes']?.length ? data['error-codes'][0] : null,
+		error: data['error-codes']?.[0] ?? 'Form validation failed'
 	};
-}
+};
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
 <p>Go back to <a href="/">home</a></p>
+<h1>{$page.error?.message}</h1>
