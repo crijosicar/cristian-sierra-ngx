6b1cab63238a2a0aa008596ae852d802c458c7ed
Cristian Sierra <crijosicar@hotmail.com>
2025-09-13 14:34:49 -0400

    added entries prerender

--- src/routes/shelf/+page.server.ts
+++ src/routes/shelf/+page.server.ts
@@ -4,3 +4,8 @@ import type { APIPaginationResponse } from '$lib/entities/APIResponse.type';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (): Promise<APIPaginationResponse<Post>> => loadPosts();
+
+export const entries = async (): Promise<string[]> => {
+	const allItems = await loadPosts();
+	return allItems.docs.map((post) => `/shelf/${post.slug}`);
+};
