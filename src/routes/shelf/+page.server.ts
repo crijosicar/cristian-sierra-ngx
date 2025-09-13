ae74b6b40b4b5845e6f10a172c7c1b2eac1b77f5
Cristian Sierra <crijosicar@hotmail.com>
2025-09-13 14:56:28 -0400

    avoid prerender

--- src/routes/shelf/+page.server.ts
+++ src/routes/shelf/+page.server.ts
@@ -3,4 +3,6 @@ import type { Post } from '$lib/entities/posts.type';
 import type { APIPaginationResponse } from '$lib/entities/APIResponse.type';
 import type { PageServerLoad } from './$types';
 
+export const prerender = false;
+
 export const load: PageServerLoad = async (): Promise<APIPaginationResponse<Post>> => loadPosts();
