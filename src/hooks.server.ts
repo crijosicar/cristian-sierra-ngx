56d5b91b7535ac036c91fe0eba7c0c9db08cf58a
Cristian Sierra <crijosicar@hotmail.com>
2026-04-19 12:50:31 -0400

    feat: add VITE_ALLOWED_HOST to environment variables and fix paraglideHandle return structure

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -10,13 +10,14 @@ export const init: ServerInit = async () => {
 	});
 };
 
-const paraglideHandle: Handle = ({ event, resolve }) =>
-	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+const paraglideHandle: Handle = ({ event, resolve }) => {
+	return paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
 		event.request = localizedRequest;
 		return resolve(event, {
 			transformPageChunk: ({ html }) =>
 				html.replace('%lang%', locale).replace('%dir%', getTextDirection(locale))
 		});
 	});
+};
 
 export const handle: Handle = sequence(paraglideHandle);
